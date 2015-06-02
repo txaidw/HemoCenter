296918223e7b2b377ffd6754bbeda9410b3aff49
Txai Wieser <txaidw@gmail.com>
2015-06-02 00:04:38 -0300

    slitview

---
+++ HemoCenter/HemoCenter/MasterTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MasterTableViewController.swift
+//  HemoCenter
+//
+//  Created by Txai Wieser on 01/06/15.
+//  Copyright (c) 2015 TDW. All rights reserved.
+//
+
+import UIKit
+
+class MasterTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view delegate
+
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = UIColor.clearColor()
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        (view as! UITableViewHeaderFooterView).alpha = 1
+        (view as! UITableViewHeaderFooterView).textLabel.textColor = UIColor(red:0.4, green:0.08, blue:0.07, alpha:1)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HemoCenter/HemoCenter/SplitViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SplitViewController.swift
+//  HemoCenter
+//
+//  Created by Txai Wieser on 01/06/15.
+//  Copyright (c) 2015 TDW. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red:0.4, green:0.08, blue:0.07, alpha:1)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
