0fb0c243b251060d871c87454728d04ee65d7888
zzanette <alysson.zanette@hotmail.com>
2015-06-03 03:34:52 -0300

    Criando API

---
+++ api/includes/Config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alyssonzanette
+ * Date: 6/3/15
+ * Time: 1:43 AM
+ */
+
+define('DB_HOST', "localhost");
+define('DB_USER', "root");
+define('DB_PASSWORD', "root");
+define('DB_DATABASE', "hemocentro");
+
+
+?>
